6304e7fe510db86d910af0bff9fc6250ebccd8c7
JS5806 <leejeasang123@gmail.com>
2026-01-12 00:29:16 +0900

    Delete src directory

--- src/main/java/com/company/lunchvote/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.lunchvote.repository;
-
-import com.company.lunchvote.domain.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
