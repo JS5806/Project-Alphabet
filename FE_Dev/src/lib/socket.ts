8259c14e16bb92de23730b9bcb48583ef3e5ec71
JS5806 <leejeasang123@gmail.com>
2026-01-12 10:08:10 +0900

    Delete FE_Dev/src directory

--- FE_Dev/src/lib/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io, Socket } from 'socket.io-client';
-import { ServerToClientEvents, ClientToServerEvents } from '../types';
-
-const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, {
-  autoConnect: false,
-  transports: ['websocket'], // MVP 성능 최적화
-});
