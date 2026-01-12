b5b2953b7c275083a1726575fe024e4d15a1b72d
JS5806 <leejeasang123@gmail.com>
2026-01-12 09:07:24 +0900

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// 스타일 병합 유틸리티 (Tailwind 충돌 방지)
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
